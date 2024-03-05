0e567913baf71a09af935bc69eac9b45c81065e6
Na Yang <nay@fb.com>
2024-03-05 13:45:24 -0800

    Initial spring boot/eclipse project created.

---
+++ src/main/java/org/mike/dynamic/controllers/MainController.java
@@ -0,0 +1,13 @@
+package org.mike.dynamic.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping(path = "/hello")
+	public String hello() {
+		return "Hello!";
+	}	
+}
